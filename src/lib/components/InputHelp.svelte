3ec4bfa8bdb2a9429e482122510994f3b3546057
Maanlex <alexandre@moquet.com>
2024-06-23 16:28:07 +0200

    feat: add input helper component

---
+++ src/lib/components/InputHelp.svelte
@@ -0,0 +1,11 @@
+<style lang="postcss">
+    .kbd{
+        @apply text-secondary-content;
+    }
+</style>
+<div>
+    <p class="prose mb-0">
+        Press <kbd class="kbd kbd-xs">Esc</kbd> to cancel
+        , <kbd class="kbd kbd-xs">Enter</kbd> to confirm.
+    </p> 
+</div>
